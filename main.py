0a8024dbd6204328173acf5174ce5fc822954dcc
Zi Chong <zichong@New-Hires-MacBook-Air-3.local>
2014-09-14 00:29:32 -0700

    Changed from stream to preprepared analysis

--- main.py
+++ main.py
@@ -3,70 +3,100 @@ import numpy as np
 import cv2
 from skimage import measure
 from collections import Counter
-from collections import deque
 
-
-def capture_masks(video_clip, count=5, show_vid=False, verbose=True):
-    assert count != 0, 'Must capture at least 1 mask'
-    
-    cap = cv2.VideoCapture(video_clip)
-    if verbose:
-        print "video access: complete"
-    fgbg = cv2.BackgroundSubtractorMOG()
-    
-    mask_storage = deque(maxlen=count)
-    while(1):
-        ret, frame = cap.read()
-        if frame is None:
-            # video has ended
-            break
-        
-        fgmask = fgbg.apply(frame)
-        if len(mask_storage) == count:  # full
-            mask_storage.popleft()
-        mask_storage.append(fgmask)
-        
-        if show_vid:
-            cv2.imshow('frame', fgmask)
-            k = cv2.waitKey(30) & 0xff
-            if k == 27:  # esc to stop 
-                break
-    
-    if verbose:
-        print "video analysis: complete"
-    cap.release()
-    cv2.destroyAllWindows()    
-    
-    result = list(mask_storage)
-    return result
+def color_func(blob_id, people_blob_ids):
+    ''' Color the blobs identified as people blue. '''
+    if blob_id = 0:  # background
+        return [0, 0, 0]  # black
+    if blob_id in people_blob_ids:
+        return [0, 0, 255]  # blue
+    else:  # non-person blob
+        return [255, 255, 255] # white
 
 
 def v1_ppl_counter(mask, min_size, max_size):
     labelled_mask = measure.label(mask)
     sizer = Counter([item for row in labelled_mask for item in row])
-    result = 0
+
+    people_blob_ids = []
     for blob_id, size in sizer.items():
         if size >= min_size and size <= max_size:
-            result += 1
-    return result
+            people_blob_ids.append(blob_id)
+
+    colored_mask = np.array([[color_func(pixel, people_blob_ids)
+                                for pixel in row] for row in labelled_mask])
+    return len(people_blob_ids), colored_mask
 
 
-def agg_ppl_counter(masks, count_func, agg_func=np.mean):
-    return agg_func(map(count_func, list(masks)))
+def process_mask(fgbg, frame):
+    fgmask = fgbg.apply(frame)
+    count, colored_mask = v1_ppl_counter(fgmask, 20, 100)
+    return count, colored_mask
+
+
+def process_video(in_path,
+                  out_path,
+                  seconds,
+                  show_vid=False,
+                  verbose=True):
+    ''' Creates mask video and generates counts. '''
+
+    assert agg_lvl > 0, 'Must aggregate at least 1 frame'
+
+    # initialize everything
+    if verbose:
+        print "Accessing video %s" % in_path
+    cap = cv2.VideoCapture(video_clip)
+
+    fgbg = cv2.BackgroundSubtractorMOG()
+    frame_counter = 1  # start at 1 because first frame used for sizing
+    count_storage = []
+
+    if verbose:
+        print "Sizing first frame"
+    ret, frame = cap.read()
+    height, width, layers = frame.shape
+    video = cv2.VideoWriter(out_path, -1, 1, (width,height))
+    count, mask = process_mask(fbgb, frame)
+    video.write(mask)
+    count_storage.append(count)
+
+    if verbose:
+        print "Commencing frame-by-frame analysis"
+
+    while(1):
+        ret, frame = cap.read()
+        if frame is None:
+            # video has ended
+            break
+
+        frame_counter += 1
+        count, mask = process_mask(fbgb, frame)
+        video.write(mask)
+        count_storage.append(count)
+
+        if show_vid:
+            cv2.imshow('frame', fgmask)
+            k = cv2.waitKey(30) & 0xff
+            if k == 27:  # esc to stop 
+                break
+
+    if verbose:
+        print "Closing connections"
+    cap.release()
+    video.release()
+    cv2.destroyAllWindows()
+
+    return count_storage
 
 
 
+# Insert some data
 # data comes in a dict of {locname: 'video_path', longitude, latitude}
 # output {locname: time, count}
 data = {}
 data['tsrobo1'] = ('testclip.mp4', 40.7577, 73.9857) #http://www.earthcam.com/usa/newyork/timessquare/?cam=tsrobo1
 
-def prep_counts(data, out_path):
-
-    for locname, details in data:
-        video_path,_,_ = details
-        masks = capture_masks('testclip2.mp4')
-        count_func = lambda x: v1_ppl_counter(x, 20, 100)
-        agg_ppl_counter(masks, count_func)
-
+for locname, details in data:
+    
 	
